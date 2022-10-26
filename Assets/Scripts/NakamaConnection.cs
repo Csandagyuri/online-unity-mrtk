369417660a2ef127cdb206753cc65edd90e9b1c1
gyorgy.csanda <gyorgy.csanda@lensa.com>
2022-10-26 21:20:47 +0200

    upgraded gitignore for mrtk, added mathcmaker, tested it on 2 app, on 1 pc, removed sharedasset0.assets

--- Assets/Scripts/NakamaConnection.cs
+++ Assets/Scripts/NakamaConnection.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using Nakama;
 
@@ -14,6 +15,8 @@ public class NakamaConnection : MonoBehaviour
     private ISession session;
     private ISocket socket;
 
+    private string ticket;
+
     // Start is called before the first frame update
     async void Start()
     {
@@ -22,8 +25,30 @@ public class NakamaConnection : MonoBehaviour
         socket = client.NewSocket();
         await socket.ConnectAsync(session, true);
 
+        socket.ReceivedMatchmakerMatched += OnReceivedMatchmakerMatched;
+
         Debug.Log(session);
         Debug.Log(socket);
 
     }
+
+    public async void FindMatch()
+    {
+         Debug.Log("Finding match!");
+
+        var matchmakerTicket = await socket.AddMatchmakerAsync("*", 2, 2);
+        ticket = matchmakerTicket.Ticket;
+
+    }
+
+    private async void OnReceivedMatchmakerMatched(IMatchmakerMatched matchmakerMatched)
+    {
+        var match = await socket.JoinMatchAsync(matchmakerMatched);
+        
+        Debug.Log("Our Session ID:" + match.Self.SessionId);
+        foreach (var user in match.Presences)
+        {
+            Debug.Log("Connected User Session ID:" + user.SessionId);
+        }
+    } 
 }
